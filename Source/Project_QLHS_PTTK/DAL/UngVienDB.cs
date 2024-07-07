1b46f108d0e55b6284a06a3cc53b7e1d8a49c9c1
Trinhtrinh18 <151738716+Trinhtrinh18@users.noreply.github.com>
2024-07-07 07:02:08 +0700

    Update UngVienDB.cs

--- Source/Project_QLHS_PTTK/DAL/UngVienDB.cs
+++ Source/Project_QLHS_PTTK/DAL/UngVienDB.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -39,5 +39,51 @@ namespace DAL
             }
             return dtuv;
         }
+
+
+
+        // Phương thức kiểm tra tài khoản đã tồn tại
+        public static bool IsAccountExists(OracleConnection connection, string hovaten)
+        {
+            try
+            {
+                using (OracleCommand command = new OracleCommand("SELECT COUNT(*) FROM Candidates WHERE HoVaTen = :HoVaTen", connection))
+                {
+                    command.Parameters.Add(new OracleParameter("HoVaTen", hovaten));
+                    connection.Open();
+                    int count = Convert.ToInt32(command.ExecuteScalar());
+                    connection.Close();
+                    return count > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi kiểm tra tài khoản: " + ex.Message);
+            }
+        }
+
+        // Phương thức thêm ứng viên vào cơ sở dữ liệu
+        public static void AddCandidate(OracleConnection connection, string hovaten, DateTime ngaysinh, string diachi, string sodienthoai, string password)
+        {
+            try
+            {
+                using (OracleCommand command = new OracleCommand("INSERT INTO Candidates (HoVaTen, NgaySinh, DiaChi, SoDienThoai, MatKhau) VALUES (:HoVaTen, :NgaySinh, :DiaChi, :SoDienThoai, :MatKhau)", connection))
+                {
+                    command.Parameters.Add(new OracleParameter("HoVaTen", hovaten));
+                    command.Parameters.Add(new OracleParameter("NgaySinh", ngaysinh));
+                    command.Parameters.Add(new OracleParameter("DiaChi", diachi));
+                    command.Parameters.Add(new OracleParameter("SoDienThoai", sodienthoai));
+                    command.Parameters.Add(new OracleParameter("MatKhau", password));
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi thêm ứng viên: " + ex.Message);
+            }
+        }
     }
 }
+  
