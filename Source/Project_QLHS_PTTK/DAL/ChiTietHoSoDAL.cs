33a154d7ff85b282b97a8c7f97212d2be72b2a96
HungRepublic <165574124+HungRepublic@users.noreply.github.com>
2024-07-07 14:10:22 +0700

    Add files via upload

---
+++ Source/Project_QLHS_PTTK/DAL/ChiTietHoSoDAL.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System;
+using Oracle.ManagedDataAccess.Client;
+
+namespace DAL
+{
+    public static class ChiTietHoSoDAL
+    {
+        public static DataTable TraCuuChiTietHoSo(OracleConnection conn, string condition)
+        {
+            string sql = @"
+                SELECT STT, LoaiHoSo, TenHoSo
+                FROM ADMIN.ChiTietHoSo"
+                + condition;
+
+            DataTable dataTable = new DataTable();
+            try
+            {
+                using (OracleCommand cmd = new OracleCommand(sql, conn))
+                {
+                    using (OracleDataAdapter da = new OracleDataAdapter(cmd))
+                    {
+                        da.Fill(dataTable);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return dataTable;
+        }
+    }
+}
+
---
+++ Source/Project_QLHS_PTTK/DAL/TraCuuHoSoDAL.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+using System;
+using Oracle.ManagedDataAccess.Client;
+
+namespace DAL
+{
+    public static class HoSoDAL
+    {
+        public static DataTable TraCuuHoSo(OracleConnection conn, string condition)
+        {
+            string sql = "SELECT MaHoSo, NgayNopHoSo, TinhTrangHS FROM ADMIN.HoSoUngTuyen " + condition;
+            DataTable dataTable = new DataTable();
+            try
+            {
+                using (OracleCommand cmd = new OracleCommand(sql, conn))
+                {
+                    using (OracleDataAdapter da = new OracleDataAdapter(cmd))
+                    {
+                        da.Fill(dataTable);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return dataTable;
+        }
+    }
+}
