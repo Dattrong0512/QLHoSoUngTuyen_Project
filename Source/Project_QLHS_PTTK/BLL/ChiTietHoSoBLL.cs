f91561da2fbb1f03712c092b02a153230dd61238
HungRepublic <165574124+HungRepublic@users.noreply.github.com>
2024-07-07 14:11:27 +0700

    Add files via upload

---
+++ Source/Project_QLHS_PTTK/BLL/ChiTietHoSoBLL.cs
@@ -0,0 +1,16 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System.Data;
+using System;
+
+namespace BLL
+{
+    public static class ChiTietHoSoBLL
+    {
+        public static DataTable TraCuuChiTietHoSo(OracleConnection conn, string maHoSo)
+        {
+            string condition = $" WHERE MaHoSo = '{maHoSo}'";
+            return DAL.ChiTietHoSoDAL.TraCuuChiTietHoSo(conn, condition);
+        }
+    }
+}
+
---
+++ Source/Project_QLHS_PTTK/BLL/TraCuuHoSoBLL.cs
@@ -0,0 +1,19 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System.Data;
+
+namespace BLL
+{
+    public static class HoSoBLL
+    {
+        public static DataTable TraCuuHoSo(OracleConnection conn, string maHoSo = "")
+        {
+            string condition = "";
+            if (!string.IsNullOrEmpty(maHoSo))
+            {
+                condition = $" WHERE MaHoSo LIKE N'%{maHoSo}%'";
+            }
+
+            return DAL.HoSoDAL.TraCuuHoSo(conn, condition);
+        }
+    }
+}
